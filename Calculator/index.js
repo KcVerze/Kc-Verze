83d5116eb24842ae30b10cad0ce4512ff55b2702
KcVerze <kcverze@gmail.com>
2025-05-10 16:15:56 +0100

    calculator done

--- Calculator/index.js
+++ Calculator/index.js
@@ -1,3 +1,21 @@
-function toScreen(value) {
-    document.getElementsById('screen').value += value;
+// to screen
+function ToScreen(value) {
+    document.getElementById('screen').value += value;
+}
+
+// clear screen
+
+function clearScreen() {
+    document.getElementById('screen').value = '';
+}
+
+// equal/ evaluate values
+
+function equal() {
+    try {
+        document.getElementById('screen').value = 
+        eval(document.getElementById('screen').value);
+    } catch {
+        document.getElementById('screen').value = 'Error';
+    }
 }
